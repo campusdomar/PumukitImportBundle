e039d476e9b08091ca633d3d427ec833806e1f48
mrey <mrey@teltek.es>
2017-03-09 20:48:44 +0100

    TTK-13526 php cs fixer

--- Services/ImportMultimediaObjectService.php
+++ Services/ImportMultimediaObjectService.php
@@ -466,4 +466,27 @@ class ImportMultimediaObjectService extends ImportCommonService
 
         return $multimediaObject;
     }
+
+    private function fixStatusWithPublicationChannel(MultimediaObject $multimediaObject)
+    {
+        $multimediaObject->setStatus(intval($multimediaObject->getStatus()));
+
+        $publishedInAll = $multimediaObject->containsAllTagsWithCodes(array('PUCHWEBTV', 'PUCHMOODLE', 'PUCHYOUTUBE'));
+        $publishedInWebTVAndYoutube = $multimediaObject->containsAllTagsWithCodes(array('PUCHWEBTV', 'PUCHYOUTUBE'));
+        $publishedInWebTVAndMoodle = $multimediaObject->containsAllTagsWithCodes(array('PUCHWEBTV', 'PUCHMOODLE'));
+        $publishedOnlyInYoutube = $multimediaObject->containsAllTagsWithCodes(array('PUCHYOUTUBE'));
+        $publishedOnlyInMoodle = $multimediaObject->containsAllTagsWithCodes(array('PUCHMOODLE'));
+
+        $statusNotPublished = ($multimediaObject->getStatus() !== MultimediaObject::STATUS_PUBLISHED);
+
+        if (($publishedInAll || $publishedInWebTVAndYoutube || $publishedInWebTVAndMoodle) && $statusNotPublished) {
+            $multimediaObject->setStatus(MultimediaObject::STATUS_PUBLISHED);
+            $webTVTag = $this->tagRepo->findOneByCod('PUCHWEBTV');
+            $removeTags = $this->tagService->removeTag($multimediaObject, $webTVTag, false);
+        } elseif (($publishedOnlyInYoutube || $publishedOnlyInMoodle) && $statusNotPublished) {
+            $multimediaObject->setStatus(MultimediaObject::STATUS_PUBLISHED);
+        }
+
+        return $multimediaObject;
+    }
 }
