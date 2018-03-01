7e3fe76de94ad45f67183401f1eaa5420b471acc
pogando <pablo.ogando@teltek.es>
2018-03-01 16:59:37 +0100

    TTK-17203 use php cs fixer

--- Tests/Services/ImportMaterialServiceTest.php
+++ Tests/Services/ImportMaterialServiceTest.php
@@ -76,7 +76,7 @@ class ImportMaterialServiceTest extends WebTestCase
     private function importXMLFile($filePath, $multimediaObject)
     {
         $xml = simplexml_load_file($filePath, 'SimpleXMLElement', LIBXML_NOCDATA);
-        if ($xml === false) {
+        if (false === $xml) {
             throw new \Exception('Not valid XML file: '.$filePath);
         }
 
